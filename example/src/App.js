dc664b4c9d5702d3f2abfc47b3a00300526a9847
sarbjeetsingh <sarbjeet.singh@addvalsolutions.com>
2021-05-29 18:19:11 +0530

    initial commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,45 @@
 import React from 'react'
 
-import { ExampleComponent } from 'emo-rating-react'
+import { EmoRating } from 'emo-rating-react'
 import 'emo-rating-react/dist/index.css'
-
+const ratingData = [
+  {
+    id:1,
+    text:"Terrible",
+    icon:"https://s3-ap-southeast-1.amazonaws.com/kulcare-assets/images/terrible-icon.svg",
+    icon_selected:"https://s3-ap-southeast-1.amazonaws.com/kulcare-assets/images/terrible-big-icon.svg",
+ },
+ {
+  id:2,
+  text:"Bad",
+  icon:"https://s3-ap-southeast-1.amazonaws.com/kulcare-assets/images/bad-icon.svg",
+  icon_selected:"https://s3-ap-southeast-1.amazonaws.com/kulcare-assets/images/sad-big-icon.svg"
+},
+{
+  id:3,
+  text:"Okay",
+  icon:"https://s3-ap-southeast-1.amazonaws.com/kulcare-assets/images/okay-icon.svg",
+  icon_selected:"https://s3-ap-southeast-1.amazonaws.com/kulcare-assets/images/okay-big-icon.svg"
+},
+{
+  id:4,
+  text:"Good",
+  icon:"https://s3-ap-southeast-1.amazonaws.com/kulcare-assets/images/good-icon.svg",
+  icon_selected:"https://s3-ap-southeast-1.amazonaws.com/kulcare-assets/images/happy-big-icon.svg"
+},
+{
+  id:5,
+  text:"Great",
+  icon:"https://s3-ap-southeast-1.amazonaws.com/kulcare-assets/images/great-icon.svg",
+  icon_selected:"https://s3-ap-southeast-1.amazonaws.com/kulcare-assets/images/great-big-icon.svg"
+},
+]
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+
+  const handleOnChange =  ( selectedRating,e)=>{
+    console.log(selectedRating)
+  }
+  return <EmoRating ratingData ={ratingData} onChange={handleOnChange} defaultSelected={1} />
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const EmoRating = ({ ratingData,onChange,defaultSelected}) => {
+  const [selectedRating, setSelectedRating] = useState(defaultSelected ? defaultSelected:null);
+
+  // when user clicks on rating
+  const handleRatingClick = (rating,e)=>{
+    setSelectedRating(rating.id);
+    if(onChange){
+      onChange(rating,e)
+    }
+  }
+
+  return (
+          <div className={styles['rating-wrapper']}>
+              {/* <div className={styles['bg-line']}></div> */}
+              {ratingData && ratingData.length>0 ? (
+              ratingData.map((rating)=>(
+                <div className={styles['face']} key={rating.id}>
+                  <div className={`${styles['rating-btn']} ${selectedRating && selectedRating===rating.id ? styles['selected']:""}`}>
+                    <input type="button" style={{backgroundImage:`url(${selectedRating && selectedRating===rating.id ? rating.icon_selected:rating.icon })`}} onClick={(e)=>handleRatingClick(rating,e)} />
+                    <div className={styles['rating-text']}>{rating.text}</div>
+                  </div>
+                </div>
+              )) 
+              ):(
+                <div>No data available</div>
+              ) }
+              
+               
+            </div>
+  )
 }
