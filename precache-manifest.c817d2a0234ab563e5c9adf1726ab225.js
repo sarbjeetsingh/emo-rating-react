c9a26dd7e256875922964a9adb52adc5204d9f2e
sarbjeetsingh <sarbjeet.singh@addvalsolutions.com>
2021-06-06 21:06:56 +0530

    Updates

---
+++ precache-manifest.c817d2a0234ab563e5c9adf1726ab225.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4cda71a1d8e04ee9969322c5b11993b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5bc5bbd678c4078deba9",
+    "url": "./static/css/main.70e7d8b8.chunk.css"
+  },
+  {
+    "revision": "a6318b4d62d71c27cec7",
+    "url": "./static/js/2.00d33c49.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.00d33c49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bc5bbd678c4078deba9",
+    "url": "./static/js/main.914dcee6.chunk.js"
+  },
+  {
+    "revision": "f0e7208b76ada9847179",
+    "url": "./static/js/runtime-main.e0cbd001.js"
+  }
+]);
